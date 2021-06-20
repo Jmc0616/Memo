784e5cdb35ca77da906f1e647a63be21eb08de12
JMC <jmch0616@gmail.com>
2021-06-20 15:24:07 +0900

    Initial Commit

---
+++ jmcMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jmcMemo
+//
+//  Created by myeongcheol jeong on 2021/06/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
