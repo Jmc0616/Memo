4c799935da29926d71c7b0f364b25647cffa2c1d
JMC <jmch0616@gmail.com>
2021-06-20 18:30:57 +0900

    first commit

---
+++ jmcMemo/Model/model.swift
@@ -0,0 +1,22 @@
+//
+//  model.swift
+//  jmcMemo
+//
+//  Created by myeongcheol jeong on 2021/06/20.
+//
+
+import Foundation
+
+class Memo {
+    var content : String
+    var insertDate : Date
+    
+    init (content : String){
+        self.content = content
+        insertDate = Date()
+    }
+    static var dummyMemoList = [
+        Memo(content: "JMC"),
+        Memo(content: "sub JMC")
+    ]
+}
