b2ac89266c9042f35cb586b1a2667de170bb9bb1
JMC <jmch0616@gmail.com>
2021-06-21 22:14:54 +0900

    add func

--- jmcMemo/ComposeViewController.swift
+++ jmcMemo/ComposeViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var memoText: UITextView!
+    
+    @IBAction func save(_ sender: Any) {
+        guard let memo = memoText.text, memo.count > 0 else {
+            alert(message: "메모를 입력해주세요!")
+            return
+        }
+        let newMemo = Memo(content: memo)
+        Memo.dummyMemoList.append(newMemo)
+        
+        NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,4 +45,8 @@ class ComposeViewController: UIViewController {
     }
     */
 
+}
+
+extension ComposeViewController {
+    static let newMemoDidInsert = Notification.Name(rawValue: "newMemoDidInsert")
 }
