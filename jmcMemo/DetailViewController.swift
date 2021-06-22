f31368d24ad09d842d5090f5899b05c446414e42
JMC <jmch0616@gmail.com>
2021-06-22 16:52:31 +0900

    detailview

--- jmcMemo/DetailViewController.swift
+++ jmcMemo/DetailViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    var memo : Memo?
+    
+    let formatter : DateFormatter = {
+        let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .short
+        f.locale = Locale(identifier: "Ko_kr")
+        
+        return f
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,4 +37,32 @@ class DetailViewController: UIViewController {
     }
     */
 
+}
+
+extension DetailViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
+            
+            cell.textLabel?.text = memo?.content
+            
+            return cell
+            
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
+            
+            cell.textLabel?.text = formatter.string(for: memo?.insertDate)
+            
+            return cell
+        default:
+            fatalError()
+        }
+    }
+    
+    
 }
